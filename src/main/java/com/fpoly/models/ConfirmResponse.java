c6c14df4eb4a92c59c74c85760857f197e521fd6
namnt04 <nguyenthenam0401@gmail.com>
2024-12-02 16:06:34 +0700

    modeles

---
+++ src/main/java/com/fpoly/models/ConfirmResponse.java
@@ -0,0 +1,10 @@
+package com.fpoly.models;
+
+import com.fpoly.constants.ConfirmRequestType;
+
+public class ConfirmResponse extends Response {
+    private Long amount;
+    private Long transId;
+    private String requestId;
+    private ConfirmRequestType requestType;
+}
